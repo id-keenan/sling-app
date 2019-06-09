c3c44c8cfd6a42e7b3469dcd03e27b6a544c305c
Bjorn Breckenridge <bjornbreck@gmail.com>
2019-06-09 10:34:49 -0600

    catch up to master and add more styles

--- ui/src/main/resources/jcr_root/etc/clientlibs/sling-app/form.js
+++ ui/src/main/resources/jcr_root/etc/clientlibs/sling-app/form.js
@@ -1,14 +1,79 @@
-document.getElementById('appt-creator--form').addEventListener('submit', postData)
 const apptModal = document.querySelector('.appt-created--modal'),
     apptLink = apptModal.querySelector('.review-new-appt'),
-    apptCloseModal = apptModal.querySelector('.close-modal')
+    apptCloseModal = apptModal.querySelector('.close-modal'),
+    apptFormInputs = ['location-destination','location-pickup','policy-num','date-pickup','time-pickup','time-dropoff','driver','cost']
 
+// let resetForm = apptFormInputs.map((inputVal) => {
+//     let temp = document.getElementById(inputVal)
+//     return temp.value = ''
+// })
+
+class formUtils {
+    constructor(arrInputs) {
+        this.arrInputs = arrInputs
+    }
+    checkForm() {
+        for (let i = 0, len = this.arrInputs.length; i < len; i++) {
+            let temp = document.getElementById(this.arrInputs[i]).value.trim()
+            temp ? console.log('shit') : console.log('fuck')
+            // if (temp) {
+            //     input.dataset.state = 'valid'
+            // } else {
+            //     input.dataset.state = 'invalid'
+            // }
+        }
+    }
+    clearForm() {
+        for (let i = 0, len = this.arrInputs.length; i < len; i++) {
+            let temp = document.getElementById(this.arrInputs[i])
+            temp.value = ''
+        }
+    }
+}
+
+// const input = document.querySelector('.form-field input')
+// input.addEventListener('input', evt => {
+//     const value = input.value
+//     if (!value) {
+//         input.dataset.state = ''
+//         return
+//     }
+//     const trimmed = value.trim()
+//     if (trimmed) {
+//         input.dataset.state = 'valid'
+//     } else {
+//         input.dataset.state = 'invalid'
+//     }
+// })
+
+let resetFormInputs = function(arr) {
+    for (let i = 0, len = arr.length; i < len; i++) {
+        let temp = document.getElementById(arr[i])
+        temp.value = ''
+    }
+}
+// let testFunc = function(arr) {
+//     for (let i = 0, len = arr.length; i < len; i++) {
+//         // let temp = document.getElementById(arr[i])
+//         let temp = document.getElementById(arr[i]).value.trim()
+//         temp ? console.log('shit') : console.log('fuck')
+//     }
+// }
+//
+// testFunc(apptFormInputs)
+
+document.getElementById('appt-creator--form').addEventListener('submit', postData)
 apptCloseModal.addEventListener('click', closeModal)
 
 function closeModal() {
     event.preventDefault()
     apptModal.close()
 }
+
+document.addEventListener('click', function(event) {
+    if (event.target.closest('.appt-created--modal')) return;
+    apptModal.close()
+});
 
 function postData(event) {
     event.preventDefault()
@@ -22,6 +87,7 @@ function postData(event) {
             newPath = newPath.replace(/"/gm, '')
             apptLink.href = `${newPath}.html`
             apptModal.showModal()
+            resetFormInputs(apptFormInputs)
         }))
         .catch((err)=> console.log(err))
 }
