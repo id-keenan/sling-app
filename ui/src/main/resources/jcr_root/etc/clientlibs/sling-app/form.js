e9c055ad6ea6ed2e6ed1d40ae9a9c3c9717e9335
Bjorn Breckenridge <bjornbreck@gmail.com>
2019-06-08 20:46:33 -0600

    Theme create new appt, add modal for redirection

--- ui/src/main/resources/jcr_root/etc/clientlibs/sling-app/form.js
+++ ui/src/main/resources/jcr_root/etc/clientlibs/sling-app/form.js
@@ -1,12 +1,27 @@
-document.getElementById('postData').addEventListener('submit', postData)
+document.getElementById('appt-creator--form').addEventListener('submit', postData)
+const apptModal = document.querySelector('.appt-created--modal'),
+    apptLink = apptModal.querySelector('.review-new-appt'),
+    apptCloseModal = apptModal.querySelector('.close-modal')
+
+apptCloseModal.addEventListener('click', closeModal)
+
+function closeModal() {
+    event.preventDefault()
+    apptModal.close()
+}
 
 function postData(event) {
     event.preventDefault()
 
     fetch('/appointment-creator', {
         method: 'POST',
-        body: new FormData(document.getElementById('appointment-creator--form'))
+        body: new FormData(document.getElementById('appt-creator--form'))
     })
-    .then((res) => res)
-    .catch((err)=> console.log(err))
+        .then((res) => res.json().then((data) => {
+            let newPath = JSON.stringify(data.pagePath)
+            newPath = newPath.replace(/"/gm, '')
+            apptLink.href = `${newPath}.html`
+            apptModal.showModal()
+        }))
+        .catch((err)=> console.log(err))
 }
